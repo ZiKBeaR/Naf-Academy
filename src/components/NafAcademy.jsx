9ba1b1177d29d07e3ae725c6a23b28adb4032cc6
zik.bear.9@gmail.com <you@example.com>
2022-10-27 01:04:26 +0900

    Naf Academy

---
+++ src/components/NafAcademy.jsx
@@ -0,0 +1,42 @@
+ import React from "react";
+ import './NavBar.css';
+ import './Head.css';
+
+
+
+ const NafAcademy = () => {
+  return (
+    <div>
+
+
+    <div className="head-backg">
+    <div className="head-backg-img">
+
+{/* -----------------------------------Nav Bar------------------------------------------------ */}
+
+      <div className="NavBar">
+        <div className="Naf-logo"></div>
+        <div className="navbar-text">
+          <div>Kurslar</div>
+          <div className="text-center-line"></div>
+          <div>Biz bilan aloqa</div>
+        </div>
+      </div>    
+
+{/* -------------------------------Head------------------------------------------------ */}
+
+      <div className="head-text">
+        <div className="head-yellow-text">Zamonaviy kasblarni oʻrganib hayotingni</div>
+        <div className="head-big-text">180° ga</div>
+        <div className="head-yellow-text">oʻzgartir</div>
+        <div className="head-login-btn">Ro’yhatdan o’tish</div>
+      </div>
+
+      </div>
+    </div>
+
+
+    </div>
+)}
+
+export default NafAcademy
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import NafAcademy from './components/NafAcademy';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <NafAcademy/>
+  </React.StrictMode>
+);
