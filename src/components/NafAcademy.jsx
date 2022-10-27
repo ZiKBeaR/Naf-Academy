35c3a23991fbc254cd34c6a3cbe4d55147f67a05
zik.bear.9@gmail.com <you@example.com>
2022-10-27 22:55:47 +0900

    Naf Academy

--- src/components/NafAcademy.jsx
+++ src/components/NafAcademy.jsx
@@ -33,6 +33,55 @@
       </div>
 
       </div>
+
+      <div className="backg-stars-img">
+        <div className="backg-stars-text">
+        <div className="yellow-line"></div>
+        <div>
+          <div className="white-box">Statistika ma’lumotlariga qaraganda, dunyoda <pre> 60%  ODAM O’ZI YOQTIRMAGAN </pre> ishda ishlashga majbur bo‘lar ekan.</div>
+          <div className="white-text">BUNING ASOSIY SABABLARI:</div>
+        </div>
+        </div>
+
+        <div className="yellow-boxs">
+        <div className="yellow-box">
+          <div className="money"></div>
+          <div className="yellow-box-text">Daromadning kamligi</div>
+        </div>
+        <div className="yellow-box">
+          <div className="headcas"></div>
+          <div className="yellow-box-text">O‘g‘ir jismoniy mehnat</div>
+        </div>
+        <div className="yellow-box">
+          <div className="stress"></div>
+          <div className="yellow-box-text">Noqulay ish sharoiti</div>
+        </div>
+      </div>
+
+      <div className="question">
+        <div className="question-text">Albatta, oramizda ko‘pchilikda <div> USHBU MUAMMO BOR</div> yoki <div>KAFOLATLANMAGAN</div></div>
+        <div className="question-text width-text"> <div> TA’LIM DARGOHLARINI BITIRIB,</div> bu muammolarga <div> DUCH KELISH EHTIMOLI</div></div>
+        <div className="question-text width-text">mavjud. Hammamizda bir savol tug‘iladi:</div>
+        <div className="question-text-yellow-end">BUNGA YECHIM BORMI?</div>
+      </div>
+
+      </div>
+
+
+    </div>
+
+    <div>
+        
+        <div className="backg-img-mauntain">
+          <div className="text-black-backg">
+            <div className="yellow-texts-black-bckg">Albatta, bugun <div> ZAMONAVIY KASBLARNI O’RGANISH </div> orqali ushbu muammollarga</div>
+            <div className="yellow-texts-black-bckg">barham berish mumkin. Lekin bugun zamonaviy kasblarni <div> O’RGANISH </div></div>
+            <div className="yellow-texts-black-bckg"> <div> AYRIMLAR UCHUN QIMMATLIK </div> qiladi. <div>ARZON KURSLAR </div> esa <div> SIFATSIZ.</div> </div>
+            <div className="yellow-texts-black-bckg">Aynan yuqoridagi holatlarni bartaraf etish uchun <div> NAF IT ACADEMY</div></div>
+            <div className="yellow-texts-black-bckg"> sizga <div>YORDAM BERADI.</div></div>
+          </div>
+        </div>
+
     </div>
 
 
