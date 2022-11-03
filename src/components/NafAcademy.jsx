840e3646c29433f2c23d5f390483e761e9bbe2cb
zik.bear.9@gmail.com <you@example.com>
2022-11-04 00:30:09 +0900

    Naf Academy

--- src/components/NafAcademy.jsx
+++ src/components/NafAcademy.jsx
@@ -1,6 +1,8 @@
  import React from "react";
  import './NavBar.css';
  import './Head.css';
+ import './AktualKurslar.css';
+ import './Oqituvchilar.css';
 
 
 
@@ -34,28 +36,31 @@
 
       </div>
 
+{/* ---------------------------backg-stars-img---------------------------- */}
+
       <div className="backg-stars-img">
+
         <div className="backg-stars-text">
-        <div className="yellow-line"></div>
-        <div>
-          <div className="white-box">Statistika ma’lumotlariga qaraganda, dunyoda <pre> 60%  ODAM O’ZI YOQTIRMAGAN </pre> ishda ishlashga majbur bo‘lar ekan.</div>
-          <div className="white-text">BUNING ASOSIY SABABLARI:</div>
-        </div>
+          <div className="yellow-line"></div>
+            <div>
+            <div className="white-box">Statistika ma’lumotlariga qaraganda, dunyoda <pre> 60%  ODAM O’ZI YOQTIRMAGAN </pre> ishda ishlashga majbur bo‘lar ekan.</div>
+            <div className="white-text">BUNING ASOSIY SABABLARI:</div>
+          </div>
         </div>
 
         <div className="yellow-boxs">
-        <div className="yellow-box">
-          <div className="money"></div>
-          <div className="yellow-box-text">Daromadning kamligi</div>
-        </div>
-        <div className="yellow-box">
-          <div className="headcas"></div>
-          <div className="yellow-box-text">O‘g‘ir jismoniy mehnat</div>
-        </div>
-        <div className="yellow-box">
-          <div className="stress"></div>
-          <div className="yellow-box-text">Noqulay ish sharoiti</div>
-        </div>
+          <div className="yellow-box">
+            <div className="money"></div>
+            <div className="yellow-box-text">Daromadning kamligi</div>
+          </div>
+          <div className="yellow-box">
+            <div className="headcas"></div>
+            <div className="yellow-box-text">O‘g‘ir jismoniy mehnat</div>
+          </div>
+          <div className="yellow-box">
+            <div className="stress"></div>
+            <div className="yellow-box-text">Noqulay ish sharoiti</div>
+          </div>
       </div>
 
       <div className="question">
@@ -69,6 +74,8 @@
 
 
     </div>
+
+{/* ---------------------------backg-img-mauntain-------------------------------- */}
 
     <div>
         
@@ -84,6 +91,82 @@
 
     </div>
 
+{/* -------------------------------AktualKurslar----------------------------- */}
+
+    <div>
+      <div className="aktual-kurslar">
+      <div>AKTUAL KURSLAR</div>
+      </div>
+
+      <div className="aktual-kurslar-left-right-displey">
+        <div className="aktual-kurslar-left">
+          <div className="aktual-kurslar-left-btn">
+            <div className="Marketing btns">Marketing</div>
+            <div className="Grafik-dizayn btns">Grafik dizayn</div>
+            <div className="web-dizayn btns">Web dizayn</div>
+          </div>
+          <div className="aktual-kurslar-left-texts">
+          <div className="kurs-name">Marketing</div>
+          <div className="yellow-black-text"><div>Agar siz grafik dizayn sohasini kuchli o'rgansangiz</div>
+          <div className="aktual-kurslar-left-text-yellow">umringizni oxirigacha pul haqida qayg'urmaysiz.</div></div>
+          <div>Grafik dizayn 4 oy davomiyligi 3 oy nazariy amaliy 1 oy faqat amaliy.
+Dars davomida photoshop,illustrator,figma dasturlari o’rgatiladi
+Smm post yaratish,logo brending, web dizayn yonalishlari o’rgatiladi (qo’shimcha poligrafik dizayn,qadoq dizayni,tipografik dizayn boshlang’ich darajada o’rgatiladi).Texnalogiylardan: shriftlar bilan ishlash,ranglar garmoniyasi,dizayn texnalogiyasi,dizaynda uslub va dizayn nazaryasi to’liq o’rgatiladi.</div>
+          <div className="aktual-kurslar-left-bold-text">Siz kansultatsiyadan ro'yxatdan o'tishingiz mumkin bu mutlaqo bepul.Bu kansultatsiyada siz pulga sotib olib bo'lmaydigan ma'lumotlarni olasiz.Kansultatsiyamiz yoqmasa bir tin to'lamaysiz va kansultatsiyadan keyin bizdan sotib olishga majbur bo'lmaysiz.</div>
+        </div>
+        </div>
+
+        <div className="aktual-kurslar-right">
+          <div className="video-player"></div>
+        </div>
+      </div>
+
+    </div>
+
+    {/* --------------------------O'qituvchilarimiz bilan tanishing------------------ */}
+
+      <div className="oqituvchilar">
+        <div className="oqituvchilar-left oqituvchilar-btn"><div className="oqituvchilar-btn-left-icon"></div></div>
+        
+        <div className="oqituvchilar-center">
+          <div className="head-text-oqituvchilar-bilan-tanishish">
+              <div className="yellow-text head-text-size">O’QITUVCHILARIMIZ </div>
+              <div>BILAN TANISHING</div>            
+          </div>
+
+          <div className="tanishuv-window">
+            <div className="tanishuv-left">
+              <div className="tanishuv-yonalishi">
+                <div className="black-line"></div>
+                <div className="yonalish-text-size">Grafik & web dizayner</div>
+              </div>
+              <div className="oqituvchi-name">Mironshoh Nasimov</div>
+              <div className="oqituvchi-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lacus, neque, mauris, nam neque. Sapien morbi nisi, etiam duis at ac amet dictum amet. Nisi amet amet vel diam ridiculus habitasse dictum. Quam tincidunt suspendisse amet lectus sed vitae tempus cras. Enim mi quam ut nulla vitae, magna neque, sollicitudin. </div>
+              
+              <div className="oqituvchi-infos-cards">
+                <div className="oqituvchi-info-card-text">
+                  <div className="yellow-number">3+</div>
+                  <div className="card-small-text">Yillik tajriba</div>
+                </div>
+                <div className="oqituvchi-info-card-text">
+                  <div className="yellow-number">125+</div>
+                  <div className="card-small-text">Loyihalar</div>
+                </div>
+                <div className="oqituvchi-info-card-text">
+                  <div className="yellow-number">50+</div>
+                  <div className="card-small-text">O’quvchilar</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="tanishuv-right">
+              <div className="oqituvchi-img"></div>
+            </div>
+          </div>
+        </div>
+        
+        <div className="oqituvchilar-right oqituvchilar-btn"><div className="oqituvchilar-btn-right-icon"></div></div>
+      </div>
 
     </div>
 )}
